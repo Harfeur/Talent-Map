849bafcf57da0a47e4b48bb989fb0517ae77c41d
gautier <gautierjperez@gmail.com>
2019-12-16 17:08:16 +0100

    tri

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/', express.static(__dirname + '/public/'));
 app.use('/packages', express.static(__dirname + '/node_modules/'));
 
 app.get('/personnels', function(req, res) {
-	let sql = 'SELECT * FROM Personnels';
+	let sql = 'SELECT * FROM Personnels ORDER BY libelle ASC';
 	db.all(sql, [], (err, rows) => {
 		if (err) {
 			throw err;
